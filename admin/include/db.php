bae402277565540c6f05c727085526c95f30be79
Hxtreme <rwpttech@gmail.com>
2026-08-25 16:43:55 +0530

    config

--- admin/include/db.php
+++ admin/include/db.php
@@ -1,9 +1,17 @@
 <?php
 date_default_timezone_set("Asia/Calcutta");
-require_once('setting.php');
-$conn = new mysqli($_host, $_username, $_password,$_database);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}	
-define('BASEURL',$baseurl);
+
+$settingFile = __DIR__ . '/setting.php';
+
+if (file_exists($settingFile)) {
+    require_once($settingFile);
+
+    $conn = new mysqli($_host, $_username, $_password, $_database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    define('BASEURL', $baseurl);
+}
 ?>

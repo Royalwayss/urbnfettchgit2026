a163112def01bfbe4b2afe216508d8e8ecd19b53
Hxtreme <rwpttech@gmail.com>
2026-08-25 16:24:01 +0530

    config

---
+++ include/footer.php
@@ -0,0 +1,525 @@
+<footer class="urban-footer">
+
+    <!-- CTA Section -->
+    <div class="container">
+        <div class="urban-footer-cta">
+
+            <div class="row align-items-center">
+
+                <div class="col-12 col-lg-4 col-xl-4 border-right-cta">
+                    <div class="urban-footer-cta-left">
+                        <div class="urban-footer-cta-icon">
+                            <img src="assets/images/urban/flask.png">
+                        </div>
+
+                        <div>
+                            <h2>Let's Create</h2>
+                            <h3>Cleaner Solutions.</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-4 col-xl-5 ps-lg-5">
+                    <div class="urban-footer-divider"></div>
+
+                    <p class="urban-footer-cta-text">
+                        Partner with URBNFETTCH for high-performance chemical
+                        solutions that drive efficiency, safety and a sustainable future.
+                    </p>
+                </div>
+
+                <div class="col-12 col-lg-4 col-xl-3 text-lg-center">
+                    <a href="contact.php" class="urban-footer-btn">
+                        GET IN TOUCH
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </a>
+                </div>
+
+                <!-- <div class="col-lg-2 text-end d-none d-lg-block">
+                        <img src="https://images.unsplash.com/photo-1581093458791-9f3c3900df4b?w=400"
+                            class="urban-footer-flask-img" alt="">
+                </div> -->
+            </div>
+        </div>
+    </div>
+    <!-- Main Footer -->
+    <div class="container">
+        <div class="urban-footer-main">
+            <div class="row">
+                <!-- Logo -->
+                <div class="col-12 col-md-6 col-lg-3 col-xl-3 mb-4 ">
+                    <div class="urban-footer-brand">
+
+                        <img src="assets/images/urban/logo.png" alt="logo">
+
+                        <p>
+                            Innovative chemical solutions
+                            that power industries and
+                            protect our planet.
+                        </p>
+
+                        <div class="urban-footer-social">
+<a href="https://www.facebook.com/Urbnfettch" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.linkedin.com/company/urbnfettch/?viewAsMember=true" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                            
+                            <a href="https://www.instagram.com/urbnfettch/" target="_blank"><i class="fab fa-instagram"></i></a>
+                          
+
+                        </div>
+
+                    </div>
+                </div>
+                <!-- Company -->
+                <div class="col-12 col-md-6 col-lg-2 col-xl-2 mb-4 ">
+                    <div class="urban-footer-widget">
+
+                        <h4>COMPANY</h4>
+
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="research-development.php">Research & Development</a></li>
+                            <li><a href="infrastructure.php">Infrastructure</a></li>
+                            <li><a href="market.php">Market</a></li>
+                            <li><a href="Career.php">Career</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+                <!-- Products -->
+                <div class="col-12 col-md-6 col-lg-3 col-xl-3 mb-4 ">
+                    <div class="urban-footer-widget">
+
+                        <h4>PRODUCTS</h4>
+
+                        <ul>
+                            <li><a href="anti-tack-solution.php">Anti-Tack Solution</a></li>
+                            <li><a href="textile-processing-solution.php">Textile Processing Solution</a></li>
+                            <li><a href="industrial-household-cleaning-solutions.php">Industrial & Household Cleaning Solution</a></li>
+
+                        </ul>
+
+                    </div>
+                </div>
+                <!-- Company -->
+                <!-- <div class="col-12 col-md-6 col-lg-6 col-xl-2 mb-4 ">
+                    <div class="urban-footer-widget">
+
+                        <h4>EXPLORE MORE</h4>
+
+                        <ul>
+                          
+                            <li><a href="market.php">Market</a></li>
+
+                          
+                            <li><a href="career.php">Career</a></li>
+                        </ul>
+
+                    </div>
+                </div> -->
+                <div class="col-12 col-md-6 col-lg-4 col-xl-4 mb-4 ">
+                    <div class="urban-footer-contact">
+
+                        <h4>GET IN TOUCH</h4>
+
+                        <ul>
+
+                            <li>
+                                <span><i class="fa-solid fa-location-dot"></i></span>
+                               Plot No. 126, Shankara Industrial Area, Bal Kalan, Amritsar, Punjab – 143001, India
+                            </li>
+
+                            <li>
+                                <span><i class="fa-solid fa-phone"></i></span>
+                                <a href="tel:+918753003200">+91 8753003200</a>
+                            </li>
+                            
+
+                            <li>
+                                <span><i class="fa-regular fa-envelope"></i></span>
+                                info@urbnfettch.com
+                            </li>
+
+                            <li>
+                                <span><i class="fa-solid fa-globe"></i></span>
+                                www.urbnfettch.com
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bottom Footer -->
+    <div class="urban-footer-bottom">
+
+        <div class="container">
+
+            <div class="row align-items-center">
+
+                <div class="col-md-6">
+                    <p>© 2026 URBNFETTCH. All Rights Reserved.</p>
+                </div>
+
+                <div class="col-md-6 text-md-end">
+
+                    <a href="#">Privacy Policy</a>
+                    <span>|</span>
+
+                    <a href="#">Terms & Conditions</a>
+                    <span>|</span>
+
+                    <a href="#">Sitemap</a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+
+<!-- rts footer area end -->
+<div class="rts__offcanvas">
+    <div class="rts__offcanvas__wrapper">
+        <div class="rts__offcanvas__header">
+            <div class="logo">
+                <a href="index.php"><img src="assets/images/logo/logo.svg" alt=""></a>
+            </div>
+            <div class="rts__offcanvas__toggle__close">
+                <i class="fa-solid fa-xmark"></i>
+            </div>
+        </div>
+
+        <div class="rts__offcanvas__mobile__menu">
+            <nav></nav>
+        </div>
+
+        <div class="rts__offcanvas__footer">
+            <div class="social">
+                <a href="https://www.facebook.com/Urbnfettch"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/urbnfettch/"><i class="fa-brands fa-instgram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rts__offcanvas__overlay"></div>
+<!-- back to top -->
+<button type="button" class="rts__back__top" id="rts-back-to-top">
+    <svg width="20" height="20" viewBox="0 0 13 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M7.30201 1.51317L7.29917 21.3422C7.29912 21.7057 6.97211 21.9993 6.5674 21.9993C6.16269 21.9992 5.83577 21.7055 5.83582 21.342L5.83844 3.10055L1.39753 7.08842C1.11169 7.34511 0.647535 7.34506 0.361762 7.0883C0.0759894 6.83155 0.0760493 6.41464 0.361896 6.15795L6.05367 1.04682C6.26405 0.857899 6.5773 0.802482 6.85167 0.905201C7.12374 1.00792 7.30205 1.24823 7.30201 1.51317Z"
+            fill="#FFF" />
+        <path
+            d="M12.9991 6.6318C12.9991 6.80021 12.9282 6.96861 12.7841 7.09592C12.4983 7.35261 12.0341 7.35256 11.7483 7.0958L6.05118 1.97719C5.76541 1.72043 5.76547 1.30352 6.05131 1.04684C6.33716 0.790152 6.80131 0.790206 7.08709 1.04696L12.7842 6.16557C12.9283 6.29498 12.9991 6.46339 12.9991 6.6318Z"
+            fill="#FFF" />
+    </svg>
+</button>
+<!-- back to top end -->
+
+<!-- script js -->
+<script defer src="assets/js/plugins/jquery.min.js"></script>
+<script defer src="assets/js/plugins/popper.js"></script>
+<script defer src="assets/js/plugins/bootstrap.min.js"></script>
+<script defer src="assets/js/plugins/gsap.min.js"></script>
+<script defer src="assets/js/plugins/scrolltigger.js"></script>
+<script defer src="assets/js/plugins/split-text.js"></script>
+<script defer src="assets/js/plugins/split-type.js"></script>
+<script defer src="assets/js/plugins/swiperjs.js"></script>
+<script defer src="assets/js/plugins/magnific-popup.js"></script>
+<script defer src="assets/js/plugins/odometer.js"></script>
+<script defer src="assets/js/plugins/smooth-scroll.js"></script>
+<script defer src="assets/js/plugins/wow.js"></script>
+<script defer src="assets/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+ document.addEventListener("DOMContentLoaded", function() {
+
+    if (window.innerWidth < 992) {
+
+        document.querySelectorAll(".urban-dropdown-toggle").forEach(function(item) {
+
+            item.addEventListener("click", function(e) {
+                e.preventDefault();
+
+                let parent = this.closest(".has-dropdown");
+
+                document.querySelectorAll(".has-dropdown").forEach(function(menu) {
+                    if (menu !== parent) {
+                        menu.classList.remove("active");
+                    }
+                });
+
+                parent.classList.toggle("active");
+            });
+
+        });
+
+    }
+
+});
+</script>
+<script>
+ window.addEventListener('scroll', function() {
+
+    const header = document.querySelector('.urban-header');
+
+    if (window.scrollY > 100) {
+        header.classList.add('fixed-header');
+    } else {
+        header.classList.remove('fixed-header');
+    }
+
+   });
+</script>
+<script>
+   document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelectorAll('.urban-innovation-circle').forEach(circle => {
+
+        let percent = circle.getAttribute('data-percent');
+        let progress = circle.querySelector('.urban-innovation-progress');
+
+        let radius = 60;
+        let circumference = 2 * Math.PI * radius;
+
+        progress.style.strokeDasharray = circumference;
+
+        let offset = circumference - (percent / 100) * circumference;
+
+        setTimeout(() => {
+            progress.style.strokeDashoffset = offset;
+        }, 300);
+
+    });
+
+});
+</script>
+<script>
+   const counters = document.querySelectorAll('.urban-counter');
+
+  const startCounter = (counter) => {
+
+    const target = +counter.getAttribute('data-count');
+    let count = 0;
+
+    const speed = target / 100;
+
+    const updateCount = () => {
+
+        if (count < target) {
+
+            count += speed;
+
+            counter.innerText = Math.ceil(count) + '+';
+
+            requestAnimationFrame(updateCount);
+
+        } else {
+
+            counter.innerText = target + '+';
+
+        }
+    };
+
+    updateCount();
+};
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            const counter = entry.target;
+
+            if (!counter.classList.contains('counted')) {
+
+                counter.classList.add('counted');
+                startCounter(counter);
+
+            }
+        }
+
+    });
+
+}, {
+    threshold: 0.5
+});
+
+counters.forEach(counter => {
+    observer.observe(counter);
+});
+</script>
+<!-- Minimal Ultra-Clean JavaScript -->
+<script>
+const buttons = document.querySelectorAll('.urbn-industry-we .nav-link');
+const imgEl = document.getElementById('industryImg');
+const titleEl = document.getElementById('industryTitle');
+const descEl = document.getElementById('industryDesc');
+const iconEl = document.getElementById('cardIcon');
+const bar = document.getElementById('progressBar');
+
+let current = 0,
+    timer;
+
+function setTab(index) {
+    current = index;
+    buttons.forEach((b, i) => b.classList.toggle('active', i === index));
+
+    imgEl.style.opacity = '0.2';
+    setTimeout(() => {
+        const btn = buttons[index];
+        imgEl.src = btn.dataset.img;
+        titleEl.textContent = btn.dataset.title;
+        descEl.textContent = btn.dataset.desc;
+        iconEl.className = `bi ${btn.dataset.icon} card-icon`;
+        imgEl.style.opacity = '1';
+    }, 150);
+
+    bar.classList.remove('active');
+    void bar.offsetWidth; // Reflow reset
+    bar.classList.add('active');
+}
+
+const start = () => timer = setInterval(() => setTab((current + 1) % buttons.length), 9000);
+const stop = () => {
+    clearInterval(timer);
+    bar.classList.remove('active');
+};
+
+buttons.forEach((btn, i) => btn.onclick = () => setTab(i));
+document.getElementById('industryShowcase').onmouseenter = stop;
+document.getElementById('industryShowcase').onmouseleave = () => {
+    bar.classList.add('active');
+    start();
+};
+
+start();
+</script>
+
+
+<script>
+new Swiper(".urbn-we-serve-home-slider", {
+
+    slidesPerView: 6,
+    spaceBetween: 40,
+    speed: 700,
+    loop: false,
+
+    navigation: {
+        nextEl: ".urbn-we-serve-home-next",
+        prevEl: ".urbn-we-serve-home-prev"
+    },
+
+    breakpoints: {
+
+        0: {
+            slidesPerView: 1
+        },
+
+        576: {
+            slidesPerView: 2
+        },
+
+        768: {
+            slidesPerView: 3
+        },
+
+        992: {
+            slidesPerView: 4
+        },
+
+        1200: {
+            slidesPerView: 5
+        },
+
+        1400: {
+            slidesPerView: 6
+        }
+
+    }
+
+});
+</script>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+
+    const items = document.querySelectorAll(".urban-solution-item");
+    const image = document.getElementById("urbanSolutionImage");
+
+    items.forEach(function (item) {
+
+        item.addEventListener("click", function () {
+
+            items.forEach(function (el) {
+                el.classList.remove("active");
+            });
+
+            this.classList.add("active");
+
+            image.style.opacity = "0";
+
+            setTimeout(function () {
+                image.src = item.getAttribute("data-image");
+                image.style.opacity = "1";
+            }, 200);
+
+        });
+
+    });
+
+});
+</script>
+ <script>
+  
+
+    // 5-Second Rotational Highlighting for Solutions
+    document.addEventListener('DOMContentLoaded', () => {
+      const solutionItems = document.querySelectorAll('.urbn-newproduct-sec-solution-item');
+      const container = document.getElementById('urbnNewProductSecSolutionsContainer');
+      let currentIndex = 0;
+      let intervalId = null;
+
+      function highlightItem(index) {
+        solutionItems.forEach(item => item.classList.remove('is-active'));
+        if (solutionItems[index]) {
+          solutionItems[index].classList.add('is-active');
+        }
+      }
+
+      function startCycle() {
+        if (intervalId) return;
+        intervalId = setInterval(() => {
+          currentIndex = (currentIndex + 1) % solutionItems.length;
+          highlightItem(currentIndex);
+        }, 2000);
+      }
+
+      function stopCycle() {
+        clearInterval(intervalId);
+        intervalId = null;
+      }
+
+      // Initial activation
+      highlightItem(currentIndex);
+      startCycle();
+
+      // Pause automated cycle while user hovers over the section
+      if (container) {
+        container.addEventListener('mouseenter', stopCycle);
+        container.addEventListener('mouseleave', startCycle);
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ include/header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- favicon -->
+    <!-- <link rel="icon" href="assets/images/fav.svg" type="image/x-icon"> -->
+    <!-- title -->
+    <title>Urbn fettch</title>
+
+    <!-- Bootstrap Css -->
+    <link rel="preload" href="assets/css/plugins/bootstrap.min.css" as="style">
+    <!-- Fontawesome Css -->
+    <link rel="preload" href="assets/css/plugins/fontawesome.css" as="style">
+    <!-- Animate css -->
+    <link rel="preload" href="assets/css/plugins/animate.css" as="style">
+    <!-- Magnific Popup Css -->
+    <link rel="preload" href="assets/css/plugins/magnific-popup.css" as="style">
+    <!-- Odometer css -->
+    <link rel="preload" href="assets/css/plugins/odometer.css" as="style">
+    <!-- Swiper css -->
+    <link rel="preload" href="assets/css/plugins/swiper.min.css" as="style">
+    <!-- Main Style -->
+    <link rel="preload" href="assets/css/style.css?v=1.1" as="style">
+
+
+
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <!-- Fontawesome Css -->
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome.css">
+    <!-- Animate css -->
+    <link rel="stylesheet" href="assets/css/plugins/animate.css">
+    <!-- Magnific Popup Css -->
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+    <!-- Odometer css -->
+    <link rel="stylesheet" href="assets/css/plugins/odometer.css">
+    <!-- Swiper css -->
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" href="assets/css/style.css?v=2.1">
+    <link rel="stylesheet" href="assets/css/urban.css?v=3.1">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- rts preloader area start -->
+    <div class="loader-wrapper">
+        <div class="loader"></div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+    <!-- rts preloader area end -->
+    <!-- main header area -->
+<header class="urban-header">
+    <nav class="navbar urban-header-navbar">
+        <div class="container-fluid">
+            <!-- Grid Layout Wrapper -->
+            <div class="row align-items-center w-100 g-0">
+                
+                <!-- Logo Column -->
+                <div class="col-6 col-md-4 col-xl-2">
+                    <a class="navbar-brand urban-header-logo m-0" href="index.php">
+                        <img src="assets/images/urban/logo.png" alt="Logo" class="img-fluid">
+                    </a>
+                </div>
+
+                <!-- Custom Toggler Column (Visible up to 1199px) -->
+                <div class="col-6 d-flex justify-content-end urban-toggler-col">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#urbanHeaderNav" aria-controls="urbanHeaderNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+                <!-- Navigation Column -->
+                <div class="col-12 col-md-8 col-xl-10 urban-nav-col">
+                    <div class="collapse navbar-collapse urban-navbar-collapse" id="urbanHeaderNav">
+                        <ul class="navbar-nav mx-auto urban-header-menu">
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">About Us</a>
+                            </li>
+
+                            <li class="nav-item dropdown has-dropdown">
+                                <a href="javascript:void(0);" class="nav-link urban-dropdown-toggle">
+                                    Products
+                                    <i class="fa-solid fa-chevron-down ms-1"></i>
+                                </a>
+                                <ul class="sub-menu">
+                                    <li><a href="anti-tack-solution.php">Anti-Tack Solution</a></li>
+                                    <li><a href="textile-processing-solution.php">Textile Processing Solution</a></li>
+                                    <li><a href="industrial-household-cleaning-solutions.php">Industrial & Household Cleaning Solution</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="research-development.php">Research & Development</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="infrastructure.php">Infrastructure</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="market.php">Market</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="career.php">Career</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+</header>
